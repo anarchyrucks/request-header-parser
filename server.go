952b883a69b0f998a0a40d7bf894574c3df3efbb
Ashish Acharya <anarchyrucks@live.com>
2017-03-09 09:58:24 +0530

    Initial Commit

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+type Header struct {
+	Ipaddress string `json:"ipaddress"`
+	Language  string `json:"language"`
+	Software  string `json:"software"`
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", rootHandler)
+	http.Handle("/", r)
+
+	http.ListenAndServe(":3000", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	ipaddress := r.RemoteAddr
+	language := strings.Split(r.Header["Accept-Language"][0], ",")[0]
+
+	rgx := regexp.MustCompile(`\((.*?)\)`)
+	rgxResult := rgx.FindStringSubmatch(r.UserAgent())[0]
+
+	software := rgxResult[1 : len(rgxResult)-1]
+
+	json.NewEncoder(w).Encode(Header{
+		ipaddress,
+		language,
+		software,
+	})
+}
